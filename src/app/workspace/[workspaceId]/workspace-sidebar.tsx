cb37c45dcf761f4255310e015f3dadb24ed380e4
ParthibanPillay <parthibanpillay237@gmail.com>
2025-02-17 17:31:57 +0530

    add channel functionality and invite modal

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -8,9 +8,11 @@ import { UseGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-member";
 import { UserItem } from "./user-item";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 const WorkspaceSidebar = () => {
     const workspaceId = useWorkspaceId();
+    const [_open, setOpen] = useCreateChannelModal();
 
     const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
     const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
@@ -52,7 +54,7 @@ const WorkspaceSidebar = () => {
             <WorkspaceSection
                 label="channels"
                 hint="New channel"
-                onNew={() => { }}
+                onNew={member.role === "admin" ? () => setOpen(true) : undefined}
             >
                 {channels?.map((item) => (
                     <SidebarItem
